c35477e999a23c9cb4915cdd4df2e0ecbd800042
Roman Domnich <domnich4@gmail.com>
2018-08-28 16:01:53 +0200

    Added the possibility to delete all products and their suppliers.

--- app/src/main/java/com/example/android/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.inventoryapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -8,17 +9,21 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.inventoryapp.data.InventoryContract;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private InventoryAdapter inventoryAdapter;
+
+    private DialogInterface.OnClickListener dialogClickListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +53,28 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
         });
 
+        dialogClickListener = new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case DialogInterface.BUTTON_POSITIVE:
+                        try {
+                            getContentResolver().delete(InventoryContract.CONTENT_DELETE_ALL, null, null);
+                        } catch (RuntimeException e) {
+                            (Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT)).show();
+                        }
+
+                        getContentResolver().notifyChange(InventoryContract.CONTENT_URI_MAIN_VIEW, null);
+
+                        break;
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        break;
+                }
+            }
+
+        };
+
         getSupportLoaderManager().initLoader(0, null, this).forceLoad();
     }
 
@@ -74,6 +101,15 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     public void addProduct(View view) {
         startActivity(new Intent(MainActivity.this, AddProductActivity.class));
+    }
+
+    public void deleteAllProducts(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setMessage(R.string.should_all_products_be_deleted)
+                .setPositiveButton(R.string.yes, dialogClickListener)
+                .setNegativeButton(R.string.no, dialogClickListener)
+                .show();
     }
 
 }
